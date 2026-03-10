c09c6b86392fdd58f57b4b9631807dda23d43f10
unknown <niel.rav.15@gmail.com>
2026-03-10 15:57:45 -0700

    created Product Model

---
+++ models/ProductModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+{
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
+  name: { type: String, required: true},
+  image: { type: String, required: true},
+  category: { type: String, required: true},
+  description: { type: String, required: true},
+  price: { type: Number, required: true, default: 0},
+  timestamp: true
+});
