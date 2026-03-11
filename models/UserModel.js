20183ade236fd09dd6270e3413f159060436ff8c
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-11 08:57:40 -0700

    update UserModel, created Category and Cart Model

--- models/UserModel.js
+++ models/UserModel.js
@@ -6,7 +6,8 @@ const userSchema = new Schema(
 {
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
-  email: { type: String, required: true, unique: true }
+  email: { type: String, required: true, unique: true },
+  isAdmin: { type: Boolean, required: true, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
